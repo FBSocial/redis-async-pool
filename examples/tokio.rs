aabd426cfb00786d71ee6314dce15da3f0495666
allen.lee <allen.lee@idreamsky.com>
2022-08-30 11:12:15 +0800

    fix: change example

--- examples/tokio.rs
+++ examples/tokio.rs
@@ -1,21 +1,22 @@
 use std::error::Error;
-
-use redis::AsyncCommands;
+use redis_cluster_async::{ Connection, Client };
 use redis_async_pool::{RedisConnectionManager, RedisPool};
+use redis_cluster_async::redis::{AsyncCommands, ConnectionLike};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let pool = RedisPool::new(
-        RedisConnectionManager::new(redis::Client::open("redis://localhost:6379")?, true, None),
-        5,
-    );
+    println!("Hello, world!");
 
+    // Create a pool of maximum 5 connections, checked on reuse without ttl.
+    let mut client = Client::open(vec!["redis://10.100.1.36:6380","redis://10.100.1.36:6381","redis://10.100.1.36:6382"])?;
+    client.set_password("idreamsky@123");
+    let pool = RedisPool::new(RedisConnectionManager::new(client, true, None), 5, );
+
+    // get a connection with the get() async method and use it as regular redis connection
     let mut con = pool.get().await?;
     con.set(b"key", b"value").await?;
     let value: Vec<u8> = con.get(b"key").await?;
     assert_eq!(value, b"value");
-    let exists: bool = con.exists(b"key").await?;
-    println!("Key `key` exists? {}", exists);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,28 @@
 //! ## Example
 //!
 //! ```rust
-//! use redis::AsyncCommands;
+//!use std::error::Error;
+//! use redis_cluster_async::{ Connection, Client };
 //! use redis_async_pool::{RedisConnectionManager, RedisPool};
+//! use redis_cluster_async::redis::{AsyncCommands, ConnectionLike};
 //!
-//! // Create a pool of maximum 5 connections, checked on reuse without ttl.
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn Error>> {
+//!     println!("Hello, world!");
 //!
-//! let pool = RedisPool::new(
-//!     RedisConnectionManager::new(redis::Client::open("redis://localhost:6379")?, true, None),
-//!     5,
-//! );
+//!     // Create a pool of maximum 5 connections, checked on reuse without ttl.
+//!     let mut client = Client::open(vec!["redis://10.100.1.36:6380","redis://10.100.1.36:6381","redis://10.100.1.36:6382"])?;
+//!     client.set_password("idreamsky@123");
+//!     let pool = RedisPool::new(RedisConnectionManager::new(client, true, None), 5, );
 //!
-//! // get a connection with the get() async method and use it as regular redis connection
-//! let mut con = pool.get().await?;
-//! con.set(b"key", b"value").await?;
-//! let value: Vec<u8> = con.get(b"key").await?;
-//! assert_eq!(value, b"value");
+//!     // get a connection with the get() async method and use it as regular redis connection
+//!     let mut con = pool.get().await?;
+//!     con.set(b"key", b"value").await?;
+//!     let value: Vec<u8> = con.get(b"key").await?;
+//!     assert_eq!(value, b"value");
+//!
+//!     Ok(())
+//! }
 //! ```
 //!
 //! You can set a ttl for each created connection by the pool,
